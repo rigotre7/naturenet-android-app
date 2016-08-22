c9bc97e4c2462128328cdfee6bf6a4441f60e0d0
Travis Christian <WildWeazel@users.noreply.github.com>
2016-08-22 15:55:02 -0400

    #7 turn off picasso debug in release builds

--- app/src/main/java/org/naturenet/NatureNetApp.java
+++ app/src/main/java/org/naturenet/NatureNetApp.java
@@ -2,18 +2,13 @@ package org.naturenet;
 
 import android.app.Application;
 
-import com.google.firebase.database.Logger;
 import com.squareup.picasso.Picasso;
 
 public class NatureNetApp extends Application {
-    private static final String TAG = "NatureNet Application";
 
     @Override
     public void onCreate() {
         super.onCreate();
-//        Firebase.setAndroidContext(this);
-//        Firebase.getDefaultConfig().setPersistenceEnabled(true);
-//        Firebase.getDefaultConfig().setLogLevel(Logger.Level.DEBUG);
-        Picasso.with(this).setIndicatorsEnabled(true);
+        Picasso.with(this).setIndicatorsEnabled(BuildConfig.DEBUG);
     }
 }

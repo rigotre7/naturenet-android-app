6afb12c172aab4fbeec22da20078704a4be801b0
Travis Christian <WildWeazel@users.noreply.github.com>
2017-02-10 16:47:02 -0500

    child activity navigation and toolbars

--- app/src/main/java/org/naturenet/ui/ObservationActivity.java
+++ app/src/main/java/org/naturenet/ui/ObservationActivity.java
@@ -1,13 +1,10 @@
 package org.naturenet.ui;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.GridView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.common.collect.Lists;
@@ -32,11 +29,8 @@ public class ObservationActivity extends AppCompatActivity {
     static String SIGNED_USER = "signed_user";
     static String OBSERVATION = "observation";
     static String OBSERVATIONS = "observations";
-    static String EMPTY = "";
 
-    Toolbar toolbar;
     Observation selectedObservation;
-    TextView explore_tv_back, toolbar_title;
     GridView gridView;
     DatabaseReference mFirebase;
     List<Observation> observations;
@@ -47,26 +41,22 @@ public class ObservationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_observation);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar_title = (TextView) findViewById(R.id.app_bar_explore_tv);
-        explore_tv_back = (TextView) findViewById(R.id.explore_tv_back);
+        selectedObservation = getIntent().getParcelableExtra(OBSERVATION);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(true);
+            getSupportActionBar().setTitle(R.string.observation_title);
+        }
+
         gridView = (GridView) findViewById(R.id.observation_gallery);
         signed_user = getIntent().getParcelableExtra(SIGNED_USER);
         observations = getIntent().getParcelableArrayListExtra(OBSERVATIONS);
-        setSupportActionBar(toolbar);
-        toolbar.setTitle(EMPTY);
         comments = null;
 
-        explore_tv_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ObservationActivity.this.goBackToExploreFragment();
-            }
-        });
-
-        selectedObservation = getIntent().getParcelableExtra(OBSERVATION);
         goToSelectedObservationFragment();
     }
 
@@ -74,25 +64,24 @@ public class ObservationActivity extends AppCompatActivity {
     public void onBackPressed() {
         if(getFragmentManager().getBackStackEntryCount() == 0) {
             finish();
+            overridePendingTransition(R.anim.stay, R.anim.slide_down);
         } else {
             super.onBackPressed();
         }
     }
 
-    public void goBackToExploreFragment() {
-        observations = null;
-        selectedObservation = null;
-        comments = null;
-        gridView = null;
-
-        Intent resultIntent = new Intent(this, MainActivity.class);
-        setResult(Activity.RESULT_OK, resultIntent);
-        finish();
-        overridePendingTransition(R.anim.stay, R.anim.slide_down);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                overridePendingTransition(R.anim.stay, R.anim.slide_down);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void goToSelectedObservationFragment() {
-        toolbar_title.setVisibility(View.GONE);
         comments = null;
         like = null;
 
--- app/src/main/java/org/naturenet/ui/ProjectActivity.java
+++ app/src/main/java/org/naturenet/ui/ProjectActivity.java
@@ -3,48 +3,29 @@ package org.naturenet.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.TextView;
+import android.view.MenuItem;
 
 import org.naturenet.R;
 import org.naturenet.data.model.Project;
-import org.naturenet.data.model.Users;
 
 public class ProjectActivity extends AppCompatActivity {
 
     static String FRAGMENT_TAG_PROJECT_DETAIL = "project_detail_fragment";
-    static String SIGNED_USER = "signed_user";
     static String PROJECT = "project";
-    static String EMPTY = "";
-
-    Project project;
-    Toolbar toolbar;
-    Users signed_user;
-    TextView project_back, toolbar_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_project);
-        project = getIntent().getParcelableExtra(PROJECT);
+        Project project = getIntent().getParcelableExtra(PROJECT);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        project_back = (TextView) findViewById(R.id.project_back);
-        toolbar_title = (TextView) findViewById(R.id.app_bar_project_tv);
-        signed_user = getIntent().getParcelableExtra(SIGNED_USER);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setTitle(EMPTY);
-        toolbar_title.setVisibility(View.VISIBLE);
-        toolbar_title.setText(project.name);
-
-        project_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(true);
+            getSupportActionBar().setTitle(project.name);
+        }
 
         getFragmentManager().beginTransaction()
                 .add(R.id.fragment_container, ProjectDetailFragment.newInstance(project), FRAGMENT_TAG_PROJECT_DETAIL)
@@ -55,8 +36,20 @@ public class ProjectActivity extends AppCompatActivity {
     public void onBackPressed() {
         if(getFragmentManager().getBackStackEntryCount() == 0) {
             finish();
+            overridePendingTransition(R.anim.stay, R.anim.slide_down);
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                overridePendingTransition(R.anim.stay, R.anim.slide_down);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

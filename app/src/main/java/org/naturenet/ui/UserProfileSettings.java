648387f32df1115ab0bb9fe7d7349d6a2bd53e72
rigotre7 <rigotre7@gmail.com>
2017-08-17 10:57:48 -0400

    add My Account button to Profile Settings page

--- app/src/main/java/org/naturenet/ui/UserProfileSettings.java
+++ app/src/main/java/org/naturenet/ui/UserProfileSettings.java
@@ -62,7 +62,7 @@ public class UserProfileSettings extends AppCompatActivity {
     private static final int REQUEST_CODE_CAMERA = 3;
     private static final int REQUEST_CODE_GALLERY = 4;
     private EditText affiliationSpinner;
-    private TextView applyChanges;
+    private TextView applyChanges, myAccountButton;
     private EditText bio;
     private Button cameraButton;
     private CameraPhoto cameraPhoto;
@@ -121,6 +121,7 @@ public class UserProfileSettings extends AppCompatActivity {
         cameraButton = ((Button)findViewById(R.id.dialog_add_observation_b_camera));
         galleryButton = ((Button)findViewById(R.id.dialog_add_observation_b_gallery));
         gridview = ((GridView)findViewById(R.id.dialog_add_observation_gv));
+        myAccountButton = (TextView) findViewById(R.id.myAccountButton);
         pictureLayout.setVisibility(View.GONE);
 
 
@@ -172,6 +173,23 @@ public class UserProfileSettings extends AppCompatActivity {
                 });
 
                 affiliationList.show();
+            }
+        });
+
+        /**
+         * Listener for when user selects 'My Account' button.
+         */
+        myAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent userIntent = new Intent(UserProfileSettings.this, UsersDetailActivity.class);
+                userIntent.putExtra(CommunitiesFragment.USER_EXTRA, signed_user);
+
+                //Make sure current user isn't null (not signed in/lost signal)
+                if(signed_user!=null){
+                    startActivity(userIntent);
+                }else
+                    Toast.makeText(UserProfileSettings.this, "Could not load account", Toast.LENGTH_SHORT).show();
             }
         });
 

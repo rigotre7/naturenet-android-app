681d05d7a59a8d718027af76d569b9a5e9f9c490
rigotre7 <rigotre7@gmail.com>
2017-08-18 11:37:12 -0400

    remove unnecessary imports

--- app/src/androidTest/java/org/naturenet/DesignIdeasTests.java
+++ app/src/androidTest/java/org/naturenet/DesignIdeasTests.java
@@ -3,7 +3,6 @@ package org.naturenet;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.Espresso.onView;
@@ -16,18 +15,12 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 import static android.support.test.espresso.matcher.ViewMatchers.Visibility.VISIBLE;
 
-import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
 
-
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;

c65ca15e196a25f765d439ed5786d1c6dd50a04f
rigotre7 <rigotre7@gmail.com>
2017-08-17 20:37:38 -0400

    navigate expandable listview test

--- app/src/androidTest/java/org/naturenet/DrawerNavigationTest.java
+++ app/src/androidTest/java/org/naturenet/DrawerNavigationTest.java
@@ -1,11 +1,16 @@
 package org.naturenet;
 
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+
+import junit.framework.AssertionFailedError;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
@@ -15,6 +20,7 @@ import static android.support.test.espresso.contrib.DrawerActions.open;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.Visibility.VISIBLE;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -24,12 +30,14 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.naturenet.data.model.Observation;
 import org.naturenet.data.model.Project;
 import org.naturenet.ui.MainActivity;
+import org.naturenet.ui.ObservationAdapter;
 
 
 import static org.hamcrest.Matchers.allOf;
@@ -45,9 +53,7 @@ public class DrawerNavigationTest {
     static String PROJECTS = "Projects";
     static String DESIGN_IDEAS = "Design Ideas";
     static String EXPLORE = "Explore";
-    static String TRACKS = "Tracks";
     static String NATURENET = "NatureNet";
-    static String MUSHROOMS = "Mushrooms";
 
     @Rule
     public ActivityTestRule<MainActivity> mMainActivity = new ActivityTestRule<>(MainActivity.class, true, true);
@@ -106,44 +112,72 @@ public class DrawerNavigationTest {
         //click on projects link
         onView(withText(PROJECTS)).perform(click());
 
-        //click on Tracks Project
-        onView(withText(TRACKS)).perform(click());
+        Thread.sleep(2000);
 
-        //make sure we are looking at Tracks
-        onView(withId(R.id.project_tv_name)).check(matches(withText(TRACKS)));
+        String[] sites = new String[4];
+        sites[0] = "Aspen";
+        sites[1] = "Anacostia";
+        sites[2] = "Elsewhere";
+        sites[3] = "Reedy Creek";
+        int index = 0;
 
-        Thread.sleep(1000);
+        while(true){
 
-        //Open specific Observation
-        onData(allOf(is(instanceOf(Observation.class)), withName("Snowmass CO"))).perform(click());
+            //click through all the projects
+            try{
+                //if no exception is thrown, we know there is a 'show more' button on the screen
+                //Log.d("gpj3", "Going to click show more button: " + index);
+                onView(withIndex(withText("Load More"), 0)).perform(click());
 
-        //click the back button in the toolbar
-        onView(withContentDescription("Navigate up")).perform(click());
+                Thread.sleep(1000);
+            }catch (NoMatchingViewException e){
+                index++;
+                if(index == sites.length)
+                    break;
 
-        //click the back button once again
-        onView(withContentDescription("Navigate up")).perform(click());
 
-        //open the navigation drawer
-        onView(withId(R.id.drawer_layout)).perform(open());
+                try{
+                    //check to see if there are two views with the same site text displayed
+                    //if so, no exception will be thrown and we will know the user's affiliation is the same as the site we're trying to click
+                    onView(withIndex(withText(sites[index]), 1)).check(matches(isDisplayed()));
+                    //therefore, we click the second one which should be the one in the ExpandableListView
+                    onView(withIndex(withText(sites[index]), 1)).perform(click());
+                }catch (NoMatchingViewException ex){
+                    //if an exception is thrown, we know there's only one view with the text we're trying to click
+                    onView(withText(sites[index])).perform(click());
+                }
 
-        pressBack();
-        pressBack();
+                Thread.sleep(1000);
+            }
 
-        //open the mushrooms link
-        onView(withText(MUSHROOMS)).perform(click());
+        }
 
-        onView(withId(R.id.project_tv_name)).check(matches(withText(MUSHROOMS)));
 
-        onData(allOf(is(instanceOf(Observation.class)), withSiteId("rcnc"))).atPosition(0).perform(click());
-
-        onView(withContentDescription("Navigate up")).perform(click());
-        onView(withContentDescription("Navigate up")).perform(click());
     }
 
 
     /*
     CUSTOM MATCHERS
      */
+
+    public static Matcher<View> withIndex(final Matcher<View> matcher, final int index) {
+        return new TypeSafeMatcher<View>() {
+            int currentIndex = 0;
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with index: ");
+                description.appendValue(index);
+                matcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                return matcher.matches(view) && currentIndex++ == index;
+            }
+        };
+    }
+
     public static BoundedMatcher<Object, Observation> withName (final String name) {
         return new BoundedMatcher<Object, Observation>(Observation.class) {
             @Override
